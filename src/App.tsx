5f61f32d232235595f88f06655bb2e4f4431de4d
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-11 00:27:23 +0900

    feat: apply atlas2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useEffect } from "react";
 import { WebGLRenderer } from "sigma";
 import { DirectedGraph } from "graphology";
 import randomLayout from "graphology-layout/random";
+import forceAtlas2 from "graphology-layout-forceatlas2";
 
 import graphData from "./data/graph.json";
 
@@ -22,20 +23,39 @@ function App() {
     }
   }
 
+  graph.forEachNode((node) => {
+    graph.updateNodeAttribute(
+      node,
+      "size",
+      () => 2.5 + 0.8 * Math.sqrt(graph.outDegree(node))
+    );
+  });
+
   randomLayout.assign(graph);
+  forceAtlas2.assign(graph, {
+    iterations: 200,
+    settings: { gravity: 20 },
+  });
+
+  const defaultEdgeColor = "rgba(100, 100, 100, 0.5)";
 
   useEffect(() => {
     const renderer = new WebGLRenderer(graph, containerRef.current, {
+      defaultEdgeColor,
       defaultEdgeType: "arrow",
     });
+
     renderer.on("enterNode", (e) => {
       for (const edge of graph.inEdges(e.node)) {
-        console.log(edge);
         graph.updateEdgeAttribute(edge, "color", () => "#f00");
       }
-      console.log("wow");
     });
-    console.log(renderer.settings);
+
+    renderer.on("leaveNode", (e) => {
+      for (const edge of graph.inEdges(e.node)) {
+        graph.updateEdgeAttribute(edge, "color", () => defaultEdgeColor);
+      }
+    });
   });
 
   const height = document.body.clientHeight;
