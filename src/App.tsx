a0e7835c4db3585f115a475fc0c2f14997ffc325
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-10 10:08:12 +0900

    deps: add sigma, react-sigma

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Sigma, SigmaEnableWebGL, RandomizeNodePositions, RelativeSize } from "react-sigma";
 
 function App() {
+  let myGraph = {
+    nodes: [
+      { id: "n1", label: "Alice" },
+      { id: "n2", label: "Rabbit" },
+    ],
+    edges: [{ id: "e1", source: "n1", target: "n2", label: "SEES" }],
+  };
+
+  const height = document.body.clientHeight;
+
   return (
     <div>
-      Hello, world!
+      <Sigma
+        graph={myGraph}
+        style={{ height }}
+        settings={{ drawEdges: true, clone: false }}
+      >
+        <RelativeSize initialSize={15} />
+        <RandomizeNodePositions />
+      </Sigma>
     </div>
   );
 }
