2c5554cabc0849ef8fd6471c25b418dd56dfbe8d
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-11 01:03:56 +0900

    feat: add highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ import graphData from "./data/graph.json";
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
 
+  const defaultEdgeColor = "rgba(100, 100, 100, 0.5)";
+  const defaultNodeColor = "rgba(100, 100, 100)";
+
+  const defaultEdgeSize = 2.5;
+  const activeEdgeSize = 3.5;
+
   const graph = new DirectedGraph();
   for (const node of graphData.nodes) {
     graph.addNode(node.id, { label: node.label });
@@ -17,7 +23,10 @@ function App() {
 
   for (const edge of graphData.edges) {
     try {
-      graph.addEdge(edge.source, edge.target, { arrow: "target" });
+      graph.addEdge(edge.source, edge.target, {
+        arrow: "target",
+        size: defaultEdgeSize,
+      });
     } catch (e: any) {
       continue;
     }
@@ -37,24 +46,46 @@ function App() {
     settings: { gravity: 20 },
   });
 
-  const defaultEdgeColor = "rgba(100, 100, 100, 0.5)";
-
   useEffect(() => {
     const renderer = new WebGLRenderer(graph, containerRef.current, {
       defaultEdgeColor,
       defaultEdgeType: "arrow",
+      defaultNodeColor,
     });
 
     renderer.on("enterNode", (e) => {
       for (const edge of graph.inEdges(e.node)) {
-        graph.updateEdgeAttribute(edge, "color", () => "#f00");
+        graph.updateEdgeAttribute(edge, "color", () => "#ffa8a8");
+        graph.updateEdgeAttribute(edge, "size", () => activeEdgeSize);
+        graph.updateNodeAttribute(graph.source(edge), "color", () => "#ff6b6b");
       }
+
+      for (const edge of graph.outEdges(e.node)) {
+        graph.updateEdgeAttribute(edge, "color", () => "#74c0fc");
+        graph.updateEdgeAttribute(edge, "size", () => activeEdgeSize);
+        graph.updateNodeAttribute(graph.target(edge), "color", () => "#339af0");
+      }
+
+      graph.updateNodeAttribute(e.node, "color", () => "#20c997");
     });
 
     renderer.on("leaveNode", (e) => {
-      for (const edge of graph.inEdges(e.node)) {
+      for (const edge of graph.inEdges(e.node).concat(graph.outEdges(e.node))) {
         graph.updateEdgeAttribute(edge, "color", () => defaultEdgeColor);
+        graph.updateEdgeAttribute(edge, "size", () => defaultEdgeSize);
+        graph.updateNodeAttribute(
+          graph.source(edge),
+          "color",
+          () => defaultNodeColor
+        );
+        graph.updateNodeAttribute(
+          graph.target(edge),
+          "color",
+          () => defaultNodeColor
+        );
       }
+
+      graph.updateNodeAttribute(e.node, "color", () => defaultNodeColor);
     });
   });
 
