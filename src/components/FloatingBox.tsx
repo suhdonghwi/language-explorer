316a497f6f5f0a9e908575c8c1fb750b2b1ef65d
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-13 02:59:50 +0900

    chore: remove unnecessary imports

--- src/components/FloatingBox.tsx
+++ src/components/FloatingBox.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components/macro";
-import { WebGLRenderer } from "sigma";
 
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
